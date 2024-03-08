c5e99558bdf2df48634be6cdd33b3db149cfa07d
Arkashersz <extensed.wc@hotmail.com>
2024-03-08 14:18:13 -0300

    Configuring the geometric models of the three.js

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,11 +1,12 @@
-'use client'
+"use client";
 
 import { useEffect, useRef } from "react";
 
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import gsap from "gsap";
+import { gsap } from "gsap";
 import Bounded from "@/components/Bounded";
+import Shapes from "./Shapes";
 
 /**
  * Props for `Hero`.
@@ -16,15 +17,19 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
-  const component = useRef(null)
+  const component = useRef(null);
 
   useEffect(() => {
     let ctx = gsap.context(() => {
-      const tl = gsap.timeline()
+      const tl = gsap.timeline();
 
-      tl.fromTo(".name-animation", {
-        x: -100, opacity: 0, rotate: -10
-      },
+      tl.fromTo(
+        ".name-animation",
+        {
+          x: -100,
+          opacity: 0,
+          rotate: -10,
+        },
         {
           x: 0,
           opacity: 1,
@@ -35,37 +40,41 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           delay: 0.5,
           stagger: {
             each: 0.1,
-            from: "random"
+            from: "random",
           },
         }
       );
 
-      tl.fromTo(".job-title", {
-        y: 20,
-        opacity: 0,
-        scale: 1.2
-      }, {
-        opacity: 1,
-        y: 0,
-        duration: 1,
-        scale: 1,
-        ease: "elastic.out(1,0.3)"
-      });
-
-    }, component)
+      tl.fromTo(
+        ".job-title",
+        {
+          y: 20,
+          opacity: 0,
+          scale: 1.2,
+        },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          scale: 1,
+          ease: "elastic.out(1,0.3)",
+        }
+      );
+    }, component);
     return () => ctx.revert();
-  }, [])
-
+  }, []);
 
   const renderLetter = (name: KeyTextField, key: string) => {
     if (!name) return;
     return name.split("").map((letter, index) => (
-      <span key={index} className={`name-animation name-animation-${key} inline-block opacity-0`}>
+      <span
+        key={index}
+        className={`name-animation name-animation-${key} inline-block opacity-0`}
+      >
         {letter}
       </span>
-    ))
-  }
-
+    ));
+  };
 
   return (
     <Bounded
@@ -74,15 +83,29 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <Shapes />
         <div className="col-start-1 md:row-start-1">
-          <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none 
-          tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
-            <span className="block text-slate-300">{renderLetter(slice.primary.first_name, "first")}</span>
-            <span className="-mt-[.2em] block text-slate-500">{renderLetter(slice.primary.last_name, "last")}</span>
+          <h1
+            className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none 
+          tracking-tighter"
+            aria-label={
+              slice.primary.first_name + " " + slice.primary.last_name
+            }
+          >
+            <span className="block text-slate-300">
+              {renderLetter(slice.primary.first_name, "first")}
+            </span>
+            <span className="-mt-[.2em] block text-slate-500">
+              {renderLetter(slice.primary.last_name, "last")}
+            </span>
           </h1>
-          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-blue-200 
+          <span
+            className="job-title block bg-gradient-to-tr from-yellow-500 via-blue-200 
           to-yellow-500 bg-clip-text text-2x1 font-bold uppercase tracking-[.2em] 
-          text-transparent opacity-0 md:text-4x1">{slice.primary.tag}</span>
+          text-transparent opacity-0 md:text-4x1"
+          >
+            {slice.primary.tag}
+          </span>
         </div>
       </div>
     </Bounded>
